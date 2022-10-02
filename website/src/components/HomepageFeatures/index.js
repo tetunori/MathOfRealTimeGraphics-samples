574e85f66e8dfc140e8a74b01006afd9d28758a3
tetunori <tetunori@gmail.com>
2022-10-02 11:04:48 +0900

    translated top page

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import Translate from '@docusaurus/Translate';
 
 const FeatureList = [
   {
-    title: 'Easy to Play',
+    title: <Translate >'Easy to Play'</Translate>,
     Svg: require('@site/static/img/thumbs_up_flat_default.svg').default,
     description: (
       <>
-        With the book in hand, simply visit this site to view the shader samples immediately.
+        <Translate >With the book in hand, simply visit this site to view the shader samples immediately.</Translate>
       </>
     ),
   },
   {
-    title: 'Multi-device Support',
+    title: <Translate >'Multi-device Support'</Translate>,
     Svg: require('@site/static/img/laptop_flat.svg').default,
     description: (
       <>
-        With WebGL 2.0, all samples can be viewed in various environments such as PCs, smartphones, and tablets.
+        <Translate >With WebGL 2.0, all samples can be viewed in various environments such as PCs, smartphones, and tablets.</Translate>
       </>
     ),
   },
   {
-    title: 'Live editor',
+    title: <Translate >'Live editor'</Translate>,
     Svg: require('@site/static/img/flying_saucer_flat.svg').default,
     description: (
       <>
-        Now, you can EDIT glsl samples in the codeblocks!
+        <Translate >Now, you can EDIT glsl samples in the codeblocks!</Translate>
       </>
     ),
   },
